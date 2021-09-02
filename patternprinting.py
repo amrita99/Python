883f20f0600f9bcb73ea70f33fbcdd02b85099c6
Amrita MS <55478795+amrita99@users.noreply.github.com>
2021-09-02 14:26:36 -0400

    Add files via upload
    Patter printing u

---
+++ patternprinting.py
@@ -0,0 +1,18 @@
+i=1
+j=2
+while i>=1:
+    a=" "*j+"*"*i+" "*j
+    print(a)
+    i=i+2
+    j=j-1
+    if i>5:
+        break
+i=3
+j=1
+while i>=1:
+    a=" "*j+"*"*i+" "*j
+    print(a)
+    i=i-2
+    j=j+1
+    if i>5:
+        break
